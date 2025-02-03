388cec30de5d019e3c50ca3fa3f08f6716eeaf5b
Sjot0 <jotsandhu0450@gmail.com>
2025-02-03 18:16:27 +0530

    Solution to Input and Print Two Strings

--- Input and Print Two Strings@@6788d5012db25efec946f940/code.c
+++ Input and Print Two Strings@@6788d5012db25efec946f940/code.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 int main() {
-    char a, b;  // Corrected the colon (:) to a comma (,)
-    scanf("%c %c", &a, &b);
+    char a, b; 
+    scanf("%s %s", &a, &b);
 
-    printf("You entered: %c and %c", a, b);  // Added a comma between a and b
+    printf("You entered: %s and %s", a, b);  
     return 0;
 }
