1a8bd753d39c44b8abe7960e3bd6c04d5c033914
Sjot0 <jotsandhu0450@gmail.com>
2025-02-05 12:01:48 +0530

    Solution to Logical NOT and AND Combination

--- Logical NOT and AND Combination@@67882a731b4392d8fc1577e0/code.c
+++ Logical NOT and AND Combination@@67882a731b4392d8fc1577e0/code.c
@@ -1,15 +1,20 @@
 #include <stdio.h>
 
+
+
 int main() {
-    int num1, num2;  // Declare two integer variables
-    scanf("%d %d", &num1, &num2);  // Read two space-separated integers from input
-
-    // Check if neither number is greater than zero using '!' and '&&'
-    if (!(num1 > 0) && !(num2 > 0)) {
-        printf("True");  // If both are <= 0, output "True"
-    } else {
-        printf("False");  // Otherwise, output "False"
+    int num1 , num2 ;
+    scanf("%d %d", &num1 , &num2);
+    if (!(num1 > 0) && !(num2 > 0))
+    {
+        printf("True");
     }
-
-    return 0;  // Return 0 to indicate successful execution
+    else
+    {
+    printf("False");
+    }
+    return 0;
 }
+
+
+
