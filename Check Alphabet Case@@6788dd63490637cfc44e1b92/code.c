bd1998ace068aa0c83803e4b97900703b9533e5d
Sjot0 <jotsandhu0450@gmail.com>
2025-02-10 18:26:31 +0530

    Solution to Check Alphabet Case

---
+++ Check Alphabet Case@@6788dd63490637cfc44e1b92/code.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+int main() {
+    char a;
+    scanf("%c",&a);
+    if(a.isUppercase){
+        printf("Uppercase");
+    
+    }
+    else{
+    printf("Lowercase");
+    }
+    return 0;
+}
