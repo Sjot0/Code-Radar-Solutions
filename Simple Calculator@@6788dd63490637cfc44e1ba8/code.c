74e4ec3538cc203af3dca1a3df9543d68d363eba
Sjot0 <jotsandhu0450@gmail.com>
2025-02-13 00:47:23 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -1,29 +1,30 @@
 #include <stdio.h>
 
-int main() {
-    int a, b;
-    char c;  // Use a single char instead of an array
-    scanf("%d %d %c", &a, &b, &c);  // Read integers and a single character
+int main(){
+    int a,b;
+    char c;
+    scanf("%d %d %c",&a,&b,&c);
 
-    if (c == '*') {  // Corrected to use '==' for comparison
-        int product = a * b;
-        printf("%d", product);
-    } else if (c == '/') {  // Corrected to use '==' for comparison
-        if (b != 0) {  // Check for division by zero
-            int div = a / b;
-            printf("%d", div);
-        } else {
-            printf("Division by zero error");
-        }
-    } else if (c == '+') {  // Corrected to use '==' for comparison
-        int sum = a + b;
-        printf("%d", sum);
-    } else if (c == '-') {  // Corrected to use '==' for comparison
-        int sub = a - b;
-        printf("%d", sub);
-    } else {
+    if(c=='*'){
+        int product=a*b;
+        printf("%d",product);
+
+    }
+    else if(c=='/'){
+        int div=a/b;
+        printf("%d",div);
+    }
+    else if(c=='+'){
+        int sum=a+b;
+        printf("%d",sum);
+
+    }
+    else if(c=='-'){
+        int sub=a-b;
+        printf("%d",sub);
+    }
+    else{
         printf("Invalid");
     }
-
     return 0;
 }
